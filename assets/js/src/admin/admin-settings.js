8cca2caa0f3ee21f55a27cf1c3646b116998b324
Jonathan Stegall <jstegall@minnpost.com>
2022-05-04 21:31:06 -0500

    admin javascript formatting

--- assets/js/src/admin/admin-settings.js
+++ assets/js/src/admin/admin-settings.js
@@ -1,12 +1,10 @@
-var $ = window.jQuery;
-
 function showEmbedOptions(field, multiple) {
-	if ( multiple == '0' ) {
+	if (multiple === '0') {
 		$(field).parents('form').find('h2:nth-of-type(2)').hide();
 		$(field).parents('form').find('h2:nth-of-type(3)').show();
 		$(field).parents('form').find('table:nth-of-type(2)').hide();
 		$(field).parents('form').find('table:nth-of-type(3)').show();
-	} else if ( multiple == '1' ) {
+	} else if (multiple === '1') {
 		$(field).parents('form').find('h2:nth-of-type(2)').show();
 		$(field).parents('form').find('h2:nth-of-type(3)').hide();
 		$(field).parents('form').find('table:nth-of-type(2)').show();
@@ -15,17 +13,21 @@ function showEmbedOptions(field, multiple) {
 }
 
 // handle the checkbox that switches between single/multiple ads
-$(document).on('click', 'input[name="arcads_dfp_acm_provider_multiple_embeds[]"]', function() {
-	var multiple = $(this).val();
-	showEmbedOptions($(this), multiple);
-});
+$(document).on(
+	'click',
+	'input[name="arcads_dfp_acm_provider_multiple_embeds[]"]',
+	function () {
+		const multiple = $(this).val();
+		showEmbedOptions($(this), multiple);
+	}
+);
 
 // on load, check for single vs multiple embeds
-$(document).ready(function() {
-	var fieldname = 'input[name="arcads_dfp_acm_provider_multiple_embeds[]"]';
-	if ( $(fieldname).length) {
-		var field = $(fieldname);
-		var multiple = $(fieldname + ':checked').val();
+$(document).ready(function () {
+	const fieldname = 'input[name="arcads_dfp_acm_provider_multiple_embeds[]"]';
+	if ($(fieldname).length) {
+		const field = $(fieldname);
+		const multiple = $(fieldname + ':checked').val();
 		showEmbedOptions(field, multiple);
 	}
 });
